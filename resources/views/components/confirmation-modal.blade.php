b3c3c99a91d4663017e4aeec3ba4c064c76ad295
Ye Htun <156397048+yehtun-MIT@users.noreply.github.com>
2024-09-24 12:53:56 +0630

    update color for dark mode

--- resources/views/components/confirmation-modal.blade.php
+++ resources/views/components/confirmation-modal.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+    <div class="bg-white dark:bg-black px-4 pt-5 pb-4 sm:p-6 sm:pb-4 ">
         <div class="sm:flex sm:items-start">
             <div class="mx-auto shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                 <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
@@ -10,18 +10,18 @@
             </div>
 
             <div class="mt-3 text-center sm:mt-0 sm:ms-4 sm:text-start">
-                <h3 class="text-lg font-medium text-gray-900">
+                <h3 class="text-lg font-medium text-black dark:text-white">
                     {{ $title }}
                 </h3>
 
-                <div class="mt-4 text-sm text-gray-600">
+                <div class="mt-4 text-sm text-black dark:text-white">
                     {{ $content }}
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-end">
+    <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 dark:bg-black text-end">
         {{ $footer }}
     </div>
 </x-modal>
